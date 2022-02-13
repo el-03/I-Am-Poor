a1d166413d1262a5e9c98a3091ec230908d5e47a
Bilal El Bari <bilal.bari@dana.id>
2022-02-13 12:00:42 +0700

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Bilal El Bari on 13/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
